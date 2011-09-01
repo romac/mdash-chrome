f47c3c5f8de3b08bbfc54209550d60f860cb35df
Romain Ruetschi <romain.ruetschi@gmail.com>
2011-09-01 02:18:58 +0200

    Rename class View into Column and refactor it.

---
+++ js/column.js
@@ -0,0 +1,56 @@
+
+( function( exports )
+{
+    var Column = exports.Column = function( $element )
+    {
+        this.$el      = $el;
+        this.sections = null;
+    },
+    proto = Column.prototype;
+    
+    proto.render = function()
+    {
+        var _this = this,
+            html  = '';
+        
+        this.sections.forEach( function( section )
+        {
+            html += _this.renderSection( section );
+        } );
+        
+        this.$el.html( html );
+    };
+    
+    proto.renderSection = function( section )
+    {
+        var _this = this,
+            bookmarksHtml = '';
+        
+        section.children.forEach( function( bookmark )
+        {
+            bookmarksHtml += _this.renderBookmark( bookmark );
+        } );
+        
+        return ich.section( {
+            title     : section.title,
+            bookmarks : bookmarksHtml
+        } );
+    };
+    
+    proto.renderBookmark = function( bookmark )
+    {
+        var link = document.createElement( 'a' );
+        
+        link.href = bookmark.url;
+        
+        var data = {
+            id      : bookmark.id,
+            title   : bookmark.title,
+            url     : link.href,
+            favicon : 'chrome://favicon/' + link.href.origin
+        };
+        
+        return ich.bookmark( data );
+    };
+
+} )( window );
