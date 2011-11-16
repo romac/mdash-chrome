d0a5af368203e14cda0221053109e12fcb4bd3e6
Romain Ruetschi <romain.ruetschi@gmail.com>
2011-11-16 09:44:22 +0100

    Use Arial font for Windows users.
    Lekton appears to be glitchy on every version of Windows.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
 ( function( mdash )
 {
     
+    if( navigator.platform.indexOf( 'Win' ) !== -1 )
+    {
+        document.documentElement.classList.add( 'win' );
+    }
+    
     var dashboard = new mdash.Dashboard();
     
     $( document ).ready( dashboard.init.bind( dashboard ) );
