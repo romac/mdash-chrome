eea9906baa2b8fb1572bd6e4dd4dc4f308c38f5c
Romain Ruetschi <romain.ruetschi@gmail.com>
2012-01-25 00:02:20 +0100

    Add a noop; Growl-like (hahaha) system.

---
+++ js/growl.js
@@ -0,0 +1,15 @@
+
+( function( mdash, $ )
+{
+    
+    // WIP
+    
+    var Growl = mdash.Growl = {
+        
+        show : function( message, options )
+        {
+            // noop;
+        }
+    };
+    
+} )( window.mdash, Zepto );
