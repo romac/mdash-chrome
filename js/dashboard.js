b328a6c27c69f5e124c732fc6617714698717fed
Romain Ruetschi <romain.ruetschi@gmail.com>
2012-02-07 18:46:12 +0100

    Bump extension version number to 0.7.2.

--- js/dashboard.js
+++ js/dashboard.js
@@ -5,7 +5,7 @@
     var Dashboard = mdash.Dashboard = function() {},
         proto     = Dashboard.prototype;
     
-    Dashboard.VERSION = '0.7.1';
+    Dashboard.VERSION = '0.7.2';
     
     proto.init = function()
     {
