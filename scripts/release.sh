7658f2b40fec0fb6e620126025156b2ffc4a3029
Romain Ruetschi <romain.ruetschi@gmail.com>
2012-02-07 17:00:34 +0100

    Add scripts/ folder.

---
+++ scripts/release.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+OLD=$1
+NEW=$2
+
+cat js/dashboard.js | sed "s/'$OLD'/'$NEW'/g" #> js/dashboard.js
+cat manifest.json | sed s/"$OLD"/"$NEW"/g #> manifest.json
+git add js/dashboard.js manifest.json
+git commit -m "Bump version number to $NEW"
+git tag v$NEW
+git push origin master --tags
+
+git checkout gh-pages
+cat updates.xml | sed s/$OLD/$NEW/g > updates.xml
+git add updates.xml
+git commit -m "Bump latest extension version to $NEW."
+git push origin gh-pages
+
+git checkout master
