d8d22104e8dfced6afd86d5a77019918c9d962f9
Romain Ruetschi <romain.ruetschi@gmail.com>
2012-11-30 08:27:44 +0100

    Bump version number to 0.8.0.

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,12 @@
 
 ( function( mdash, $ )
 {
-    
+
     var Dashboard = mdash.Dashboard = function() {},
         proto     = Dashboard.prototype;
-    
-    Dashboard.VERSION = '0.7.7';
-    
+
+    Dashboard.VERSION = '0.8.0';
+
     proto.init = function()
     {
         this.$fontSizes  = $( '#fontctrl > a' );
@@ -15,29 +15,29 @@
         this.$getStarted = $( '#getstarted' );
         this.$bookmarks  = $( '#bookmarks' );
         this.$version    = $( '#version' );
-        
+
         this.manager  = new mdash.Manager();
         this.fontCtrl = new mdash.FontCtrl( this.$fontSizes );
         this.helpCtrl = new mdash.HelpCtrl( this.$helpCtrl, this.$getStarted, this.$bookmarks );
         this.editCtrl = new mdash.EditCtrl( this.$editBtn, this.$bookmarks );
-        
+
         this.fontCtrl.init();
         this.helpCtrl.init();
         this.editCtrl.init();
-        
+
         this.manager.init( this.loadBookmarks.bind( this ) );
-        
+
         this.setupUIKit();
     };
-    
+
     proto.setupUIKit = function()
     {
         ui.Dialog.effect = 'fade';
-        
+
         $( document ).on( 'keyup', '#dialog', function( e )
         {
             var $dialog = $( '#dialog' );
-            
+
             if( e.keyCode === 13 /* enter */ )
             {
                 $dialog.find( 'button.ok' ).click();
@@ -48,25 +48,25 @@
             }
         } );
     };
-    
+
     proto.loadBookmarks = function()
     {
         var _this = this;
-        
+
         this.leftColumn  = new mdash.Column( $( '#bookmarks > .left' ) );
         this.rightColumn = new mdash.Column( $( '#bookmarks > .right' ) );
-        
+
         this.manager.getSections( 'left', function( sections )
         {
             _this.leftColumn.sections = sections;
             _this.leftColumn.render();
         } );
-        
+
         this.manager.getSections( 'right', function( sections )
         {
             _this.rightColumn.sections = sections;
             _this.rightColumn.render();
         } );
     };
-    
+
 } )( window.mdash || ( window.mdash = {} ), window.jQuery || window.Zepto );
