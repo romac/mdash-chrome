49c1973af0785e17041e2c92080635001a682c52
Romain Ruetschi <romain.ruetschi@gmail.com>
2013-11-28 23:03:30 +0100

    Bump version number to 0.9.0.

--- js/dashboard.js
+++ js/dashboard.js
@@ -5,7 +5,7 @@
     var Dashboard = mdash.Dashboard = function() {},
         proto     = Dashboard.prototype;
 
-    Dashboard.VERSION = '0.8.1';
+    Dashboard.VERSION = '0.9.0';
 
     proto.init = function()
     {
